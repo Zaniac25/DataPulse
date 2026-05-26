ddbdf862f3d238f576d223ba9cb61c8c432dbd39
M Tarini prasad <kashinathmaharana196@gmail.com>
2026-05-26 23:35:16 +0530

    done data loading file

---
+++ data_loader.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import streamlit as st
+
+@st.cache_data
+def load_data(uploaded_file):
+    """Load and cache CSV data"""
+    return pd.read_csv(uploaded_file, low_memory=False)
+
+def initialize_session_state(df, file_name):
+    """Initialize all session state variables"""
+    if "original_df" not in st.session_state:
+        st.session_state.original_df = df.copy()
+    
+    if "working_df" not in st.session_state or st.session_state.get("current_file") != file_name:
+        st.session_state.working_df = df.copy()
+        st.session_state.current_file = file_name
+    
+    if "cleaning_completed" not in st.session_state:
+        st.session_state.cleaning_completed = False
+    
+    if "cleaning_log" not in st.session_state:
+        st.session_state.cleaning_log = []
+    
+    if "warning_log" not in st.session_state:
+        st.session_state.warning_log = []
+    
+    if "outlier_cols" not in st.session_state:
+        st.session_state.outlier_cols = []
